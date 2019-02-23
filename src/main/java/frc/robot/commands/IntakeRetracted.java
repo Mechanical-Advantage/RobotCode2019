9cf5fb67fd853729e12ea79da786760767e4e9ee
abbydillon <abbydillon@users.noreply.github.com>
2019-02-23 08:42:55 -0600

    Added intake extended and retracted, small changes to toggleIntake.

--- src/main/java/frc/robot/commands/IntakeRetracted.java
+++ src/main/java/frc/robot/commands/IntakeRetracted.java
@@ -15,43 +15,25 @@ import frc.robot.RobotMap.RobotType;
 /**
  * Add your docs here.
  */
-public class ToggleIntakeRetracted extends InstantCommand {
-
-  public ToggleIntakeRetracted() {
+public class IntakeRetracted extends InstantCommand {
+  /**
+   * Add your docs here.
+   */
+  public IntakeRetracted() {
     super();
-    requires(Robot.intake);
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
   }
 
-  // Called just before this Command runs the first time
+  // Called once when the command executes
   @Override
   protected void initialize() {
-    if (RobotMap.robot == RobotType.ROBOT_2019 || RobotMap.robot == RobotType.ROBOT_2019_2) {
+
+        if (RobotMap.robot == RobotType.ROBOT_2019 || RobotMap.robot == RobotType.ROBOT_2019_2) {
       if (Robot.intake.getRetractedLeft() && Robot.intake.getRetractedRight()) {
-        Robot.intake.intake();
-        Robot.intake.setRetracted(false);
-      } else {
         Robot.intake.setRetracted(true);
-      }
+      } 
     }
   }
 
-  // Called repeatedly when this Command is scheduled to run
-  @Override
-  protected void execute() {
-  }
-
-  // Called once after timeout
-  @Override
-  protected void end() {
-    Robot.intake.stop();
-  }
-
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
-  @Override
-  protected void interrupted() {
-    end();
-  }
 }
