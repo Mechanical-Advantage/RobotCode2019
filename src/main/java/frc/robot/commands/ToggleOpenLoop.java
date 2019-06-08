fb4ad7a0fea6167c2c0a21908edd681e871bc9f6
e-bonner <e-bonner@users.noreply.github.com>
2019-06-08 09:40:08 -0400

    Allow toggling OL and drive disable while disabled

--- src/main/java/frc/robot/commands/ToggleOpenLoop.java
+++ src/main/java/frc/robot/commands/ToggleOpenLoop.java
@@ -21,6 +21,7 @@ public class ToggleOpenLoop extends InstantCommand {
     super();
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
+    this.setRunWhenDisabled(true);
   }
 
   // Called once when the command executes
